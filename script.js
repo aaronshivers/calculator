ed1ae3c4b3a7d6dc5b2a9f846e6914ac01b178cb
Aaron <42848750+aaronshivers@users.noreply.github.com>
2019-08-25 08:23:33 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,121 @@
-// Import Elements
-const input1 = document.getElementById('input1')
-const input2 = document.getElementById('input2')
-const inputs = document.getElementById('inputs')
-const buttons = document.getElementById('buttons')
-const result = document.getElementById('result')
+const calculator = document.querySelector('.calculator')
+const display = document.querySelector('.calculator__display')
+const keys = document.querySelector('.calculator__keys')
 
-// Sanitize Inputs
-const sanitizeIt = value => value.replace(/[^\d]/g, '')
+const getKeyType = key => {
+  const { action } = key.dataset
+  if (!action) return 'number'
+  if (
+    action === 'add' ||
+    action === 'subtract' ||
+    action === 'multiply' ||
+    action === 'divide'
+  ) return 'operator'
+  return action
+}
 
-inputs.addEventListener('input', e => {
-  input1.value = sanitizeIt(input1.value)
-  input2.value = sanitizeIt(input2.value)
-})
+const createResultString = (key, displayedNum, state) => {
+  const keyContent = key.textContent
+  const { firstValue, modValue, operator, previousKeyType } = state
+  const keyType = getKeyType(key)
 
-// Perform Calculation
-const calculate = method => (a, b) => {
-  switch (method) {
+  if (keyType === 'number') {
+    return displayedNum === '0' ||
+      previousKeyType === 'operator' ||
+      previousKeyType === 'calculate'
+      ? keyContent
+      : displayedNum + keyContent
+  }
+
+  if (keyType === 'decimal') {
+    if (!displayedNum.includes('.')) return displayedNum + '.'
+    if (previousKeyType === 'operator' || previousKeyType === 'calculate') return '0.'
+    return displayedNum
+  }
+
+  if (keyType === 'operator') {
+    return firstValue &&
+      operator &&
+      previousKeyType !== 'operator' &&
+      previousKeyType !== 'calculate'
+      ? calculate(firstValue, operator, displayedNum)
+      : displayedNum
+  }
+
+  if (keyType === 'clear') return 0
+
+  if (keyType === 'calculate') {
+    return firstValue
+      ? previousKeyType === 'calculate'
+        ? calculate(displayedNum, operator, modValue)
+        : calculate(firstValue, operator, displayedNum)
+      : displayedNum
+  }
+}
+
+const updateVisualState = (key, calculator) => {
+  const keyType = getKeyType(key)
+
+  Array
+    .from(key.parentNode.children)
+    .forEach(k => k.classList.remove('is-depressed'))
+
+  if (keyType === 'operator') key.classList.add('is-depressed')
+
+  if (keyType === 'clear' && key.textContent !== 'AC') {
+    key.textContent = 'AC'
+  }
+
+  if (keyType !== 'clear') {
+    const clearButton = calculator.querySelector('[data-action=clear]')
+    clearButton.textContent = 'CE'
+  }
+}
+
+const updateCalculatorState = (key, calculator, calculatedValue, displayedNum) => {
+  // const { action } = key.dataset
+
+  const keyType = getKeyType(key)
+  const { firstValue, previousKeyType, modValue, operator } = calculator.dataset
+
+  calculator.dataset.previousKeyType = keyType
+
+  // if (keyType === 'number') {}
+
+  // if (keyType === 'decimal') {
+  //   calculator.dataset.previousKeyType = 'decimal'
+  // }
+
+  if (keyType === 'operator') {
+    calculator.dataset.operator = key.dataset.action
+    calculator.dataset.firstValue = firstValue &&
+      operator &&
+      previousKeyType !== 'operator' &&
+      previousKeyType !== 'calculate'
+      ? calculatedValue
+      : displayedNum
+  }
+
+  if (keyType === 'clear' && key.textContent === 'AC') {
+    calculator.dataset.firstValue = ''
+    calculator.dataset.modValue = ''
+    calculator.dataset.operator = ''
+    calculator.dataset.previousKeyType = ''
+  }
+
+  if (keyType === 'calculate') {
+    calculator.dataset.modValue = firstValue &&
+      previousKeyType === 'calculate'
+      ? modValue
+      : displayedNum
+  }
+}
+
+const calculate = (n1, operator, n2) => {
+  const a = parseFloat(n1)
+  const b = parseFloat(n2)
+
+  switch (operator) {
     case 'add':
       return a + b
     case 'subtract':
@@ -24,19 +124,16 @@ const calculate = method => (a, b) => {
       return a * b
     case 'divide':
       return a / b
-    case 'clear':
-      input1.value = ''
-      input2.value = ''
-      return '0'
-    default:
-      return 'error'
   }
 }
 
-// Invoke Calculate With Method and Imported Numbers
-buttons.addEventListener('click', e => {
-  const method = e.target.id
-  const a = parseFloat(input1.value)
-  const b = parseFloat(input2.value)
-  result.innerHTML = calculate(method)(a, b)
+keys.addEventListener('click', e => {
+  if (!e.target.matches('button')) return
+  const key = e.target
+  const displayedNum = display.textContent
+  const resultString = createResultString(key, displayedNum, calculator.dataset)
+
+  display.textContent = resultString
+  updateCalculatorState(key, calculator, resultString, displayedNum)
+  updateVisualState(key, calculator)
 })
